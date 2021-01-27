354b19885d1a98527164a94b1f611cfaa4e8681d
MrAirisssLT <77318403+MrAirisssLT@users.noreply.github.com>
2021-01-27 13:45:43 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -20,28 +20,8 @@ input.onPinPressed(TouchPin.P0, function () {
     basic.pause(1000)
     basic.showNumber(7)
 })
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . # # # .
-        . . # . .
-        . . # . .
-        . . # . .
-        . # # # .
-        `)
-    basic.pause(100)
-    basic.showIcon(IconNames.Heart)
-    basic.pause(100)
-    basic.showLeds(`
-        # . . . #
-        # . . . #
-        # . . . #
-        # . . . #
-        . # # # .
-        `)
-    basic.pause(10000)
-})
 input.onButtonPressed(Button.AB, function () {
-    basic.showNumber(1)
+    basic.showString("O, Atradai sia funkcija!")
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
